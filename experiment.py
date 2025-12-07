85a7e6850dc9a95ca932277769e9a94dbf3d4990
listenerguo <115683829+listenerguo@users.noreply.github.com>
2025-12-07 17:03:26 +0800

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -55,7 +55,7 @@ cluster_sets = {4: [2], 8: [2, 4], 12: [2, 3, 4, 6], 16: [2, 4, 8]}
 dag_types = ['DAG']
 dag_ccrs = [0.1, 0.5, 1.0, 2.0, 5.0]
 dag_indexs = 0
-repeat = 10
+repeat = 20
 
 for core_num in processors:
     slr_fenmu_lists[core_num]=[]
